ee96c7d18ed8305389ca36c3cd3bb3d4115f328a
sneg <sneg.sbg@gmail.com>
2023-01-19 23:04:12 +0300

    Programm

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// Программа выводит третью цифру заданного числа
+Console.Clear();
+Console.WriteLine("Введите любое трехзначное число");
+int number = int.Parse(Console.ReadLine()!);
+
+int num_1 = number / 100;
+int num_2 = num_1 % 10;
+
+if (num_2 > 0) Console.WriteLine(num_2);
+else Console.WriteLine("Третьего числа нет!");
